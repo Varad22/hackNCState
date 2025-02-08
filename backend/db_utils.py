cf23a8a72df3534aee9f5562e76cfd6925c7cc4f
apurv-choudhari <apurv.choudhari1@gmail.com>
2025-02-08 17:50:42 -0500

    basic db func

---
+++ backend/db_utils.py
@@ -0,0 +1,44 @@
+from flask import jsonify, request
+from pymongo import MongoClient
+
+MONGO_URI= "mongodb+srv://apchoudh:apchoudh@cluster0.dlweo96.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(MONGO_URI)
+
+def connectDB():
+    try:
+        global dbconn
+        dbconn = client.test
+        dbconn.command("ping")
+        print("Connected to MongoDB successfully!")
+        # return dbconn
+    except Exception as e:
+        print(f"MongoDB connection failed: {e}")
+
+def add_collection(collection_name):
+    try:
+        dbconn.create_collection(collection_name)
+        return jsonify({"message": f"Collection '{collection_name}' created successfully."})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+
+def insert_data(collection_name):
+    try:
+        data = request.json
+        collection = dbconn[collection_name]
+        if isinstance(data, list):
+            collection.insert_many(data)
+        else:
+            collection.insert_one(data)
+        return jsonify({"message": f"Data inserted into '{collection_name}' successfully."})
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+def get_all(collection_name):
+    try:
+        collection = dbconn[collection_name]
+        data = list(collection.find({}, {"_id": 0}))  # Exclude ObjectId from response
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
--- backend/server.py
+++ backend/server.py
@@ -1,22 +1,21 @@
 from flask import Flask
 from flask_cors import CORS
 from pymongo import MongoClient
-
+import db_utils as db
 app = Flask(__name__)
-CORS(app)  # Allow frontend to access API
-MONGO_URI= "mongodb+srv://apchoudh:apchoudh@cluster0.dlweo96.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+CORS(app)
 
-try:
-    client = MongoClient(MONGO_URI)
-    db = client.test
-    db.command("ping")
-    print("Connected to MongoDB successfully!")
-except Exception as e:
-    print(f"MongoDB connection failed: {e}")
+db.connectDB()
 
 @app.route('/')
 def home():
     return {"Home": ["home"]}
 
+@app.route("/publish", methods=["post", "get"])
+def publish():
+    return db.add_collection("test")
+    # dbconn.insert_data("test")
+    # return "SUCCESS"
+    
 if __name__ == '__main__':
     app.run(debug=True)
