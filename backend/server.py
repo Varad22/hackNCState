5981df8a03b24d541759e7131999fa10bf32d82a
apurv-choudhari <apurv.choudhari1@gmail.com>
2025-02-08 17:12:22 -0500

    db setup

--- backend/server.py
+++ backend/server.py
@@ -1,8 +1,18 @@
 from flask import Flask
 from flask_cors import CORS
+from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)  # Allow frontend to access API
+MONGO_URI= "mongodb+srv://apchoudh:apchoudh@cluster0.dlweo96.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+try:
+    client = MongoClient(MONGO_URI)
+    db = client.test
+    db.command("ping")
+    print("Connected to MongoDB successfully!")
+except Exception as e:
+    print(f"MongoDB connection failed: {e}")
 
 @app.route('/')
 def home():
