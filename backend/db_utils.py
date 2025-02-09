bb9ff88464359faeed311a389df0f27216833ae4
Varad22 <varadjs22@gmail.com>
2025-02-08 20:16:30 -0500

    dashboard

--- backend/db_utils.py
+++ backend/db_utils.py
@@ -4,9 +4,12 @@ from pymongo import MongoClient
 from dotenv import load_dotenv
 import os
 
-load_dotenv()
 
-MONGO_URI= os.getenv("MONGO_URI")
+load_dotenv()
+# username = os.getenv("USERNAME")
+# password = os.getenv("PASSWORD")
+# mongodb+srv://varad:<db_password>@cluster0.wq3fm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
+MONGO_URI= "mongodb+srv://varad:varad@cluster0.wq3fm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 client = MongoClient(MONGO_URI)
 
 def connectDB():
@@ -43,6 +46,14 @@ def get_all(collection_name):
     try:
         collection = dbconn[collection_name]
         data = list(collection.find({}, {"_id": 0}))  # Exclude ObjectId from response
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)})
+
+def get_one(collection_name, diningHall):
+    try:
+        collection = dbconn[collection_name]
+        data = list(collection.find({"dining_hall_name": diningHall}, {"_id": 0}))
         return jsonify(data)
     except Exception as e:
         return jsonify({"error": str(e)})
--- backend/server.py
+++ backend/server.py
@@ -10,12 +10,12 @@ db.connectDB()
 collection_dh = "dining_halls"
 
 @app.route('/')
-def home():
+def login():
     return {"Home": ["home"]}
 
 @app.route("/publish", methods=["post", "get"])
 def publish():
-    return db.add_collection("test")
+    return db.add_collection("dining_halls")
     # dbconn.insert_data("test")
     # return "SUCCESS"
 
@@ -24,5 +24,19 @@ def get_dining_halls():
     data = db.get_all(collection_dh)
     return data
     
+@app.route("/dashboard", methods=["get"])
+def dashboard():
+    try:
+        response = db.get_one("dining_halls", "Hillside Dining")
+        data = response.get_json()
+
+        if data and isinstance(data, list) and len(data) > 0:
+            return jsonify(data[0].get("records", []))
+        else:
+            return jsonify({"message": "No records found"}), 404
+
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
 if __name__ == '__main__':
     app.run(debug=True)
